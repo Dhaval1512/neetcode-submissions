e217902eac521ec7804d4a8fe479cb8d7a0d6564
Dhaval1512 <117357953+Dhaval1512@users.noreply.github.com>
2026-07-21 17:47:50 -0400

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    public int binary_search(int l,int r, int[] nums, int target){
+        if(l>r) 
+        {
+            return -1;
+        }
+        
+        int mid = (l + r)/2;
+
+        if(nums[mid] == target) 
+            {
+                return mid;
+            }
+        else if(nums[mid]<target){
+            return binary_search(mid +1,r,nums,target);
+        }else{
+            return binary_search(l,mid -1,nums,target);
+        }
+    }
+    
+    public int search(int[] nums, int target) {
+        return binary_search(0,nums.length-1,nums,target);
+    }
+}
